e79a48a047dc5323ff8d2f1955c5ebb93206135f
Ilse Visser <irxess@gmail.com>
2018-03-30 21:43:06 +0200

    Convert project to Python

---
+++ generate.py
@@ -0,0 +1,16 @@
+from jinja2 import Template, Environment, FileSystemLoader
+import yaml
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+template = env.get_template('recipe.html')
+
+# load variables from YAML
+recipe = open('recipes/creme_suisse.yaml', 'r')
+yaml_result = yaml.load(recipe)
+print(yaml_result.keys())
+
+output = template.render(r=yaml_result)
+with open('generated_recipe.html', 'w') as f:
+     print(output, file=f)
