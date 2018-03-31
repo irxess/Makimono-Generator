9345baf1fd91092000d121269606d94f7f5a0b1e
Simon Randby <simon.randby@gmail.com>
2018-03-31 11:15:42 +0200

    Compile less and place in appropriate dir

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import os
 from jinja2 import Template, Environment, FileSystemLoader
+import lesscpy
 import yaml
 
 file_loader = FileSystemLoader('templates')
@@ -12,8 +13,13 @@ recipe = open('recipes/creme_suisse.yaml', 'r')
 yaml_result = yaml.load(recipe)
 print(yaml_result.keys())
 
+if not os.path.isdir('publish'):
+	os.makedirs('publish')
 output = template.render(r=yaml_result)
-if not os.path.isdir("publish"):
-	os.makedirs("publish")
 with open('publish/generated_recipe.html', 'w') as f:
 	print(output, file=f)
+if not os.path.isdir('publish/css'):
+	os.makedirs('publish/css')
+compiled_css = lesscpy.compile(open('templates/main.less', 'r'))
+with open('publish/css/main.css', 'w') as f:
+	print(compiled_css, file=f)
