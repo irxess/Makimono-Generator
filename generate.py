81fa1b4578ef27db980405bd5170ae96686e82a9
Ilse Visser <irxess@gmail.com>
2018-04-02 13:10:17 +0200

    Add ingredient amounts to each step

--- generate.py
+++ generate.py
@@ -1,16 +1,41 @@
 from jinja2 import Template, Environment, FileSystemLoader
 import yaml
 
-file_loader = FileSystemLoader('templates')
-env = Environment(loader=file_loader)
+class Ingredient:
+     def __init__(self, amount):
+          self.amount = amount
+          self.rest_id = -1
 
-template = env.get_template('recipe.html')
+def add_ingredient_amounts_to_steps(yaml):
+     ingredients = dict()
+     for ingr in yaml['ingredients']:
+          ingredients[ ingr['id'] ] = Ingredient( ingr['amount'] )
+     for s in yaml['steps']:
+          for i in s['ingredients']:
+               if 'amount' in i:
+                    ingredients[i['id']].amount -= i['amount']
+               else:
+                    if ingredients[i['id']].rest_id == -1:
+                         ingredients[i['id']].rest_id = i
+                    else:
+                         print("Ingredient ", i['id'], " in ", yaml.recipe, " has multiple unspecified steps")
+                         # TODO: throw error to be caught in the going through recipes loop
+     for _,i in ingredients.items():
+          if i.rest_id != -1:
+               i.rest_id['amount'] = i.amount
 
-# load variables from YAML
-recipe = open('recipes/creme_suisse.yaml', 'r')
-yaml_result = yaml.load(recipe)
-print(yaml_result.keys())
 
-output = template.render(r=yaml_result)
-with open('generated_recipe.html', 'w') as f:
-     print(output, file=f)
+if __name__ == "__main__":
+     file_loader = FileSystemLoader('templates')
+     env = Environment(loader=file_loader)
+
+     template = env.get_template('recipe.html')
+
+     # load variables from YAML
+     recipe = open('recipes/creme_suisse.yaml', 'r')
+     yaml_result = yaml.load(recipe)
+     add_ingredient_amounts_to_steps(yaml_result)
+
+     output = template.render(r=yaml_result)
+     with open('generated_recipe.html', 'w') as f:
+          print(output, file=f)
