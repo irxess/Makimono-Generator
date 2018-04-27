f1f23c62ef04281cabe7cf9e0d0750d00d269c42
Ilse Visser <irxess@gmail.com>
2018-04-27 22:39:41 +0200

    Add ability to parse multiple files

--- generate.py
+++ generate.py
@@ -11,24 +11,29 @@ class Ingredient:
 def add_ingredient_amounts_to_steps(yaml):
     ingredients = dict()
     for ingr in yaml['ingredients']:
+        print("Amount:", ingr['amount'])
+        if ingr['amount'] == None:
+            ingr['amount'] = ''
+            print("Amount:", ingr['amount'])
         ingredients[ ingr['id'] ] = Ingredient( ingr['amount'] )
     for s in yaml['steps']:
-        for i in s['ingredients']:
-            if 'amount' in i:
-                ingredients[i['id']].amount -= i['amount']
-            else:
-                if ingredients[i['id']].rest_id == -1:
-                    ingredients[i['id']].rest_id = i
+        if 'ingredients' in s:
+            for i in s['ingredients']:
+                if 'amount' in i:
+                    ingredients[i['id']].amount -= i['amount']
                 else:
-                    print("Ingredient ", i['id'], " in ", yaml.recipe, " has multiple unspecified steps")
-                    # TODO: throw error to be caught in the going through recipes loop
+                    if ingredients[i['id']].rest_id == -1:
+                        ingredients[i['id']].rest_id = i
+                    else:
+                        print("Ingredient ", i['id'], " in ", yaml.recipe, " has multiple unspecified steps")
+                        # TODO: throw error to be caught in the going through recipes loop
     for _,i in ingredients.items():
         if i.rest_id != -1:
             i.rest_id['amount'] = i.amount
 
-
-if __name__ == "__main__":
-    recipe = open('recipes/creme_suisse.yaml', 'r')
+def generate_files_for_recipe(name):
+    #recipe = open('recipes/scones.yaml', 'r')
+    recipe = open(os.path.join('recipes', name + '.yaml'), 'r')
     yaml_result = yaml.load(recipe)
     add_ingredient_amounts_to_steps(yaml_result)
 
@@ -38,8 +43,18 @@ if __name__ == "__main__":
     env = Environment(loader=file_loader)
     template = env.get_template('recipe.html')
     output = template.render(r=yaml_result)
-    with open('publish/generated_recipe.html', 'w') as f:
+    print(name)
+    with open('publish/' + name + '.html', 'w') as f:
         print(output, file=f)
+
+
+if __name__ == "__main__":
+    for filename in os.listdir('recipes'):
+        name = filename.split('.')[0]
+        if name != 'template':
+            print("Generating files for", filename)
+            generate_files_for_recipe(name)
+    #recipe = open('recipes/creme_suisse.yaml', 'r')
 
     if not os.path.isdir('publish/css'):
         os.makedirs('publish/css')
