28c876f5be4bebfc040b7a804d8858384464e6f5
Simon Randby <simon.randby@gmail.com>
2018-12-01 10:18:49 +0100

    Fix typo

--- read_from_yaml.py
+++ read_from_yaml.py
@@ -58,8 +58,8 @@ def read_steps(yaml, recipe, ingredients):
                     if 'unit' in ingr:
                         ingredient.unit = ingr['unit']
                     step_data.refined_ingredients_used.append(ingredient)
-            if 'depencencies' in step:
-                for dep in step['depencencies']:
+            if 'dependencies' in step:
+                for dep in step['dependencies']:
                     step_data.depends_on.append( id + int(dep) )
             recipe.steps.append(step_data)
             id += 1
