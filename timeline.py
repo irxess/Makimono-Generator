ea000eafc366c3d70ef0b035a6b5ba71defb338f
Simon Randby <simon.randby@gmail.com>
2018-12-17 15:55:06 +0100

    Tineline: Extract function for finding largest y

--- timeline.py
+++ timeline.py
@@ -12,8 +12,8 @@ def find_positions(recipe):
     global x_spacing
 
     next_group = 1
-    amount_of_steps = len(recipe.steps)
-    x_spacing = (90.0/amount_of_steps)
+    number_of_steps = len(recipe.steps)
+    x_spacing = (90.0/number_of_steps)
 
     for step in recipe.steps:
         svg_node = StepSVG(x=step.id)
@@ -21,11 +21,11 @@ def find_positions(recipe):
             svg_node.group = next_group
             next_group += 1
         else:
-            lowest_group = 1024
+            largest_seen_y_value = 1024
             for child in step.depends_on:
-                if recipe.steps[child].svg.group < lowest_group:
-                    lowest_group = recipe.steps[child].svg.group
-            svg_node.group = lowest_group
+                if recipe.steps[child].svg.group < largest_seen_y_value:
+                    largest_seen_y_value = recipe.steps[child].svg.group
+            svg_node.group = largest_seen_y_value
         step.svg = svg_node
 
 def append_done_node_to_steps(steps):
@@ -56,37 +56,41 @@ def start_improve_positions(nodes):
                         safe_ys[i] = max(safe_ys[i], safe_y)
 
 
-def improve_positions(nodes, current_group, cur_index, start_y, safe_ys, end):
-    cur_node = nodes[cur_index]
-    if cur_node.svg.y > 0:
-        return cur_node.svg.y
+def improve_positions(nodes, current_group, current_node_id, start_y, safe_ys, end):
+    current_node = nodes[current_node_id]
+    if current_node.svg.y > 0:
+        return current_node.svg.y
 
+    y_used = find_largest_safe_y(start_y, nodes, safe_ys, current_group, end)
+
+    number_of_dependencies = len(current_node.depends_on)
+    if number_of_dependencies > 1:
+        current_node.svg.y = y_used
+        safe_y = y_used
+        for child in current_node.depends_on:
+            dep_group = nodes[child].svg.group
+            dep_y = improve_positions(nodes[:current_node_id], dep_group, child, safe_y, safe_ys, current_node_id+1)
+            safe_y = dep_y + 1
+            safe_ys[child] = max(safe_ys[child], safe_y)
+        safe_ys[current_node_id] = max(safe_ys[current_node_id], current_node.svg.y +1)
+    elif number_of_dependencies == 1:
+        y_used = improve_positions(nodes[:current_node_id], current_group, current_node.depends_on[0], y_used, safe_ys, current_node_id+1)
+    current_node.svg.y = y_used
+    safe_ys[current_node_id] = max(safe_ys[current_node_id], y_used+1)
+    return y_used
+
+
+def find_largest_safe_y(initial_y, nodes, safe_ys, current_group, end):
     first_in_group = 0
-    for n in nodes:
-        if n.svg.group == current_group:
+    for node in nodes:
+        if node.svg.group == current_group:
             break
         first_in_group += 1
 
-    y_used = start_y
     for y in safe_ys[first_in_group:end]:
-        if y > y_used:
-            y_used = y
-
-    number_of_dependencies = len(cur_node.depends_on)
-    if number_of_dependencies > 1:
-        cur_node.svg.y = y_used
-        safe_y = y_used
-        for child in cur_node.depends_on:
-            dep_group = nodes[child].svg.group
-            dep_y = improve_positions(nodes[:cur_index], dep_group, child, safe_y, safe_ys, cur_index+1)
-            safe_y = dep_y + 1
-            safe_ys[child] = max(safe_ys[child], safe_y)
-        safe_ys[cur_index] = max(safe_ys[cur_index], cur_node.svg.y +1)
-    elif number_of_dependencies == 1:
-        y_used = improve_positions(nodes[:cur_index], current_group, cur_node.depends_on[0], y_used, safe_ys, cur_index+1)
-    cur_node.svg.y = y_used
-    safe_ys[cur_index] = max(safe_ys[cur_index], y_used+1)
-    return y_used
+        if y > initial_y:
+            initial_y = y
+    return initial_y
 
 
 def transform_svg_xy_values(steps):
