4d57fe8e83f6004831deb6019b72cc3c226ff718
Simon Randby <simon.randby@gmail.com>
2020-04-26 15:41:15 +0200

    Upgrade pip packages script: Add more comments and debug print statements
    Should make it a bit easier to trace what's going on if the script
    itself ever needs to be updated. Also make the amount of printed
    stuff more appropriate to the different log levels.

--- upgrade-pip-packages.ps1
+++ upgrade-pip-packages.ps1
@@ -5,6 +5,11 @@ param(
 	$pipFile
 )
 
+Write-Host "Starting attempt at upgrading package versions in pip requirements file."
+Write-Host "Note that the script performs a pip upgrade in the folder where the requirements file is to try to determin whether there are newer versions."
+Write-Host "Therefore, if you use python environments it might be a very good idea to enable them before use."
+Write-Host "Note also that the script doesn't really work with packages which are specified with `">=`" for the version."
+
 if($pipFile -eq "") {
 	# Pip file was not present.
 	# Start with assuming the pip file is in the same folder as this script (and is called requirements.txt).
@@ -34,27 +39,65 @@ else{
 }
 
 $pipFileAbsolutePath = Resolve-Path $pipFile
+Write-Debug "Using [$pipFileAbsolutePath] as path to pip file"
 
 $pipFileOriginalContent = (Get-Content $pipFileAbsolutePath)
 $pipFileWithoutComments = $pipFileOriginalContent -replace "(?m)#.*`n?", ''
-$pipFileWithoutEmptyLines = $pipFileWithoutComments -match '\S'
-$pipPackageNames = $pipFileWithoutEmptyLines -replace "==.*`n?", ""
+Write-Debug "Pip file with all comments removed:"
+Write-Debug ($pipFileWithoutComments -join "`n")
 
-Write-Verbose "Installing packages using pip to determin what pip thinks are the newest packages"
-pip install -U $pipPackageNames
+# Remove all empty lines and lines only containing whitespace after removing the comments.
+# Do it in this order, because removing comments the way it's done above leaves an empty line
+$pipFileWithoutEmptyLines = $pipFileWithoutComments -match '\S'
+Write-Debug "Pip file with all empty lines removed (should now only be versioned packages):"
+Write-Debug ($pipFileWithoutEmptyLines -join "`n")
+
+# Remove all version information, so that we only pass the package names to pip,
+# and leave it to pip to pick what's the newest version.
+$pipPackageNames = $pipFileWithoutEmptyLines -replace "==.*`n?", ""
+Write-Debug "Pip file with all other information than package names removed:"
+Write-Debug ($pipPackageNames -join "`n")
+
+Write-Verbose "Installing packages using pip to determin what pip thinks are the newest packages."
+if($PSCmdlet.MyInvocation.BoundParameters["Debug"].IsPresent) {
+	pip install --upgrade $pipPackageNames
+}
+else {
+	pip install --upgrade $pipPackageNames --quiet
+}
+
 $updatedPackages = pip freeze
 $pipFileUpdatedContent = $pipFileOriginalContent
-Write-Verbose "Replacing all current package versions with package versions pip found to be current."
+Write-Verbose "Replacing all current package versions with package versions pip found to be current and add new dependencies found by PIP."
+$newNotPreviouslyInstalledPackages = ""
 Foreach($versionedPackage in $updatedPackages) {
 	$packageName = $versionedPackage -replace "==.*", ""
 	$packageVersion = $versionedPackage -replace ".*==", ""
-	Write-Host "Setting $packageName to version $packageVersion"
+	Write-Debug "Processing package ${packageName} (version ${packageVersion}) found by pip."
 
-	$pipFileUpdatedContent = $pipFileUpdatedContent -replace "(?<=$packageName==).*", $packageVersion
+	$packagePreviouslyInstalled = $pipFileUpdatedContent -match "(?<=$packageName==).*"
+	if($packagePreviouslyInstalled) {
+		Write-Debug "Setting ${packageName} to version ${packageVersion}"
+		$pipFileUpdatedContent = $pipFileUpdatedContent -replace "(?<=$packageName==).*", $packageVersion -join "`n"
+	}
+	else{
+		Write-Debug "Found new package not previously in requirements file: ${packageName} (version ${packageVersion})"
+		$newNotPreviouslyInstalledPackages += $versionedPackage + "`n"
+	}
 }
 
-Write-Verbose "Writing new packages to requirements file."
-Set-Content $pipFileAbsolutePath $pipFileUpdatedContent
+if(!($newNotPreviouslyInstalledPackages -eq "")) {
+	Write-Verbose "Adding new not previously installed packages to file."
+	Write-Debug "List of new dependencies not previously listed in requirements file:"
+	Write-Debug ($newNotPreviouslyInstalledPackages -join "`n")
+
+	$dateTimeString = Get-Date -Format "u"
+	$pipFileUpdatedContent += "`n`n# New dependencies found by pip ${dateTimeString}:`n"
+	$pipFileUpdatedContent += $newNotPreviouslyInstalledPackages
+}
+
+Write-Verbose "Writing packages and versions to requirements file."
+Set-Content -Path $pipFileAbsolutePath -Value $pipFileUpdatedContent -Encoding UTF8NoBOM -NoNewline
 
 Write-Host "Remember to test that upgraded dependencies work before checking in."
 
