8682d940abf8fc7afd86590ee47f113c5d9b4624
Simon Randby <simon.randby@gmail.com>
2020-06-09 20:48:39 +0200

    Add comment for max total time function

--- total_time_needed_for_recipe.py
+++ total_time_needed_for_recipe.py
@@ -12,6 +12,23 @@ def calculate_total_time_needed(recipe):
 
     lower_bound_needed_time = max(slowest_chain_times)
 
+def calculate_total_time_needed_for_all_steps(steps):
+    """
+    A simplistic function for calculating the time needed for completing a collection of steps,
+    assuming that all the steps are being done consecutively.
+    This is done separately from the recursive function for finding the slowest sequence of steps
+    that bounds your execution time when you can paralellize all steps perfectly.
+    The reason for the separation (and therefore "needless" extra traversal of all the steps),
+    is that it would make the calculate_time_for_slowest_chain much more complex, for very little benefit.
+    For instance, to handle the case that some steps might branch out to be used by several subsequent
+    steps, one would have to introduce the concept of coloring or marking the nodes as visited.
+    That in turn would require a new, more complex data structure to store that state, and so on.
+    """
+    time_needed = 0
+    for step in steps:
+        time_needed += step.time.active + step.time.passive
+    return time_needed
+
 def calculate_time_for_slowest_chain(step_id, steps_dictionary)
     step = steps_dictionary[step_id]
     current_step_required_time = step.time.active + step.time.passive
@@ -26,14 +43,8 @@ def calculate_time_for_slowest_chain(step_id, steps_dictionary)
     child_path_needing_most_time = max(child_times)
     return child_path_needing_most_time + current_step_required_time
 
-def calculate_total_time_needed_for_all_steps(steps):
-    """ToDo: Describe that this is done simplistic to handle cycles without a lot of complexity"""
-    time_needed = 0
-    for step in steps:
-        time_needed += step.time.active + step.time.passive
-    return time_needed
-
 def get_root_node_ids(steps):
+    """Not sure if we're ever going to actually have to depend on it, but it was fun to make!"""
     root_node_ids = set()
 
     for step in steps:
