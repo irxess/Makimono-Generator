2d9fa6562fd14fd8d4f9fc33ec62fd220ffe57ee
Simon Randby <simon.randby@gmail.com>
2020-12-14 20:07:54 +0100

    Sitemap: Fix paths when obtained on windows

--- sitemap.py
+++ sitemap.py
@@ -1,5 +1,9 @@
 import glob
 
+def fix_path_when_broken_by_windows(path):
+    path = path.replace("\\", "/")
+    return path
+
 def create_xml_url_from_glob(name, modified=""):
     url_xml = \
 f"""
@@ -17,11 +21,15 @@ def create_sitemap(recipes):
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
     # Go through files in root folder
     for filename in glob.iglob('publish/*.html', recursive=True):
-        sitemap += create_xml_url_from_glob(filename[8:])
+        path_from_web_base = filename[8:]
+        safe_path = fix_path_when_broken_by_windows(path_from_web_base)
+        sitemap += create_xml_url_from_glob(safe_path)
 
     # Go through files in the all folder
     for filename in glob.iglob('publish/all/*.html', recursive=True):
-        sitemap += create_xml_url_from_glob(filename[8:])
+        path_from_web_base = filename[8:]
+        safe_path = fix_path_when_broken_by_windows(path_from_web_base)
+        sitemap += create_xml_url_from_glob(safe_path)
 
     for recipe in recipes:
         sitemap += create_xml_url_from_glob(recipe.url, recipe.updated)
